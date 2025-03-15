3482696aa90e974ed8e08777d8744a34df69c5a5
JasurbekYoqubjonov <jasurbekdeveloper5678@gmail.com>
2025-03-15 05:13:46 +0500

    index html fileda main page dizayni tayyorlandi

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,8 @@ export default {
   content: ['./public/index.html', './src/**/*.{js,ts}'],
   theme: {
     extend: {
-      colors: {
-        primary: '#344861',
-        secondary: '#BFC6D4',
-        active: '#E2EBF3',
-        focus: '#BBDEFB'
-      },
-      fontFamily: {
-        poppins: 'Poppins'
+      fontFamily:{
+        poppins:"Poppins"
       }
     }
   }
