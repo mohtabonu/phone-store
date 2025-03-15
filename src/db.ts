59dd1ab2a0a54f6b1d46b0ee20fa4ef6ab1aa998
JasurbekYoqubjonov <jasurbekdeveloper5678@gmail.com>
2025-03-15 06:05:09 +0500

    phone boxlar objectdagi malumotlar orqali yaratildi

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-const dataPhones = {
+ export const phonesData = {
   'tecno-spark-30': {
     idName: 'tecno-spark-30',
     showName: 'Tecno Spark 30',
@@ -6,7 +6,8 @@ const dataPhones = {
     showPrice: "1 999 000 so'm",
     information: `Dolby Atmos qo‘llab-quvvatlanadigan stereodinamiklar ovoz balandligini 300%gacha oshirib, hajmli va to‘yingan tovushni ta’minlaydi. Bu video tomosha qilishda, o‘yinlarda va musiqani tinglashda chuqur taassurot hissini yaratadi.
 64 Mp SONY IMX682 sensorli va to‘rtta chaqnashli asosiy kamera istalgan yorug‘lik sharoitlarida ham aniq va yorqin suratlarni, tabiiy ranglarni va yuqori darajadagi batafsillikni saqlashga imkon beradi.`,
-    pictureLink: 'https://images.uzum.uz/csouas3vgbkpg1nllvpg/original.jpg'
+    pictureLink: 'https://images.uzum.uz/csouas3vgbkpg1nllvpg/original.jpg',
+    isAddedToCart:false
   },
   'realme-note-50': {
     idName: 'realme-note-50',
@@ -14,15 +15,17 @@ const dataPhones = {
     numberPrice: '1576000',
     showPrice: "1 576 000 so'm",
     information: `Realme Note 50 2024-yil yanvarda taqdim etilgan byudjet smartfondir. U 6,74 dyuymli IPS LCD (720 x 1600 piksel, 90Hz) ekranga ega. Protsessor sifatida Unisoc Tiger T612 (12 nm) ishlaydi. Operativ xotira 3 GB yoki 4 GB, ichki xotira esa 64 GB yoki 128 GB bo'lib, microSD orqali 2 TB gacha kengaytirish mumkin. Orqa kamerasi 13 MP + 0,08 MP, old kamerasi esa 5 MP. Smartfon 5000 mAh sig‘imli batareyaga ega bo‘lib, 10W tezkor zaryadlashni qo‘llab-quvvatlaydi. Android 13 (realme UI) tizimida ishlaydi. O‘lchamlari 167,2 x 76,7 x 7,99 mm, vazni 186 g. Yon tomonda barmoq izi skaneri, 3,5 mm audio jek va IP54 chang hamda suvga chidamlilik mavjud. Taxminiy narxi 95 AQSh dollari.`,
-    pictureLink: 'https://images.uzum.uz/cqgufhffrr8a72r5mfig/t_product_low.jpg'
+    pictureLink: 'https://images.uzum.uz/cqgufhffrr8a72r5mfig/t_product_low.jpg',
+    isAddedToCart:false
   },
   'samsung-galaxy-a-16': {
     idName: 'samsung-galaxy-a-16',
-    showName: 'Samsung Galaxy A 16',
+    showName: 'Samsung A 16',
     numberPrice: '2649000',
     showPrice: "2 649 000 so'm",
     information: `Asosiy kamera: 50 MP, f/1.8, (keng), AF 5 MP, f/2.2, (ultra keng) 2 MP, f/2.4, (makro) IP54, chang va chayqalishga qarshi Shisha old, plastik orqa, plastik ramka Display: 6.7" Super AMOLED, 90 Gz Ruxsat: 1080 x 2340 piksel (385ppi) Barcha IMEI lar rasmiy ro'yxatdan o'tgan va ro'yxatga olish faollashuvi 30 kun ichida amalga oshiriladi. Ishlab chiqaruvchi o'z mahsulotlariga 12 oylik kafolat beradi. Selfi kamera: 13 MP, f/2.0, (keng) Batareya: 5000 mA/s, olinmaydigan`,
-    pictureLink: 'https://images.uzum.uz/ct4kbjlpb7f3jk81avb0/original.jpg'
+    pictureLink: 'https://images.uzum.uz/ct4kbjlpb7f3jk81avb0/original.jpg',
+    isAddedToCart:false
   },
   'vivo-y-28': {
     idName: 'vivi-y-28',
@@ -30,7 +33,8 @@ const dataPhones = {
     numberPrice: '2999000',
     showPrice: "2 999 000 so'm",
     information: `Vivo Y28 2024-yil yanvarda taqdim etilgan. U 6,56 dyuymli 90Hz ekranga, MediaTek Helio G85 protsessoriga, 8 GB operativ va 128/256 GB ichki xotiraga ega. Orqa kamerasi 50 MP + 2 MP, old kamerasi 8 MP. 6000 mAh batareya 44W tezkor zaryadlashni qo‘llab-quvvatlaydi. Android 14 (Funtouch OS 14) tizimida ishlaydi. IP54 chang va suvga chidamli. Taxminiy narxi 180-200 AQSh dollari.`,
-    pictureLink: 'https://images.uzum.uz/cpn5tar6eisq2rkcab5g/original.jpg'
+    pictureLink: 'https://images.uzum.uz/cpn5tar6eisq2rkcab5g/original.jpg',
+    isAddedToCart:false
   },
   'honor-x8-b': {
     idName: 'honor-x8-b',
@@ -38,7 +42,8 @@ const dataPhones = {
     numberPrice: '2499000',
     showPrice: "2 499 000 so'm",
     information: `Honor X8b 2023-yil dekabrda taqdim etilgan. U 6,7 dyuymli 90Hz AMOLED ekranga, Snapdragon 680 protsessoriga, 8 GB operativ va 128/256/512 GB ichki xotiraga ega. Orqa kamerasi 108 MP + 5 MP + 2 MP, old kamerasi 50 MP. 4500 mAh batareya 35W tezkor zaryadlashni qo‘llab-quvvatlaydi. Android 13 (MagicOS 7.2) tizimida ishlaydi. NFC va 3,5 mm audio jek yo‘q. Taxminiy narxi 215 AQSh dollari.`,
-    pictureLink: 'https://images.uzum.uz/csi81o34nkdv7h9ik890/original.jpg'
+    pictureLink: 'https://images.uzum.uz/csi81o34nkdv7h9ik890/original.jpg',
+    isAddedToCart:false
   },
   'xiaomi-redmi-14-c': {
     idName: 'xiaomi-redmi-14-c',
@@ -46,7 +51,8 @@ const dataPhones = {
     numberPrice: '1899000',
     showPrice: "1 899 000 so'm",
     information: `Xiaomi Redmi 14C 2024-yil avgust oyida taqdim etilgan. U 6,88 dyuymli, 120Hz yangilanish tezligiga ega displey bilan jihozlangan. Protsessori MediaTek Helio G81 Ultra bo'lib, 6 GB yoki 8 GB operativ xotira va 128 GB yoki 256 GB ichki xotira variantlari mavjud. Asosiy kamerasi 50 MP, old kamerasi esa 13 MP. 5160 mAh sig'imli batareya tezkor quvvatlashni qo'llab-quvvatlaydi. Qurilma Android 14 operatsion tizimida ishlaydi. Taxminiy narxi 111,87 AQSh dollaridan boshlanadi.`,
-    pictureLink: 'https://images.uzum.uz/ctgi9d45j42dmkojbvgg/original.jpg'
+    pictureLink: 'https://images.uzum.uz/ctgi9d45j42dmkojbvgg/original.jpg',
+    isAddedToCart:false
   },
   'redmi-note-14-pro': {
     idName: 'redmi-note-14-pro',
@@ -54,7 +60,8 @@ const dataPhones = {
     numberPrice: '4790000',
     showPrice: "4 790 000 so'm",
     information: `Redmi Note 14 Pro 2025-yil yanvarda taqdim etilgan. U 6,67 dyuymli 120Hz AMOLED (1,5K) ekranga, Snapdragon 7s Gen 3 protsessoriga, 8/12/16 GB RAM va 128/256/512 GB ichki xotiraga ega. Orqa kamerasi 200 MP, old kamerasi 16 MP. 5500 mAh batareya 67W tezkor zaryadlashni qo‘llab-quvvatlaydi. Android 14 (HyperOS) tizimida ishlaydi.`,
-    pictureLink: 'https://images.uzum.uz/cumrhmtht56sc95ejejg/original.jpg'
+    pictureLink: 'https://images.uzum.uz/cumrhmtht56sc95ejejg/original.jpg',
+    isAddedToCart:false
   },
   'honor-200-lite': {
     idName: 'honor-200-lite',
@@ -62,6 +69,7 @@ const dataPhones = {
     numberPrice: '2999000',
     showPrice: "2 999 000 so'm",
     information: `Honor 200 Lite 2024-yil aprel oyida taqdim etilgan. U 6,7 dyuymli, 90Hz yangilanish tezligiga ega AMOLED ekranga ega. Protsessori MediaTek Dimensity 6080 bo'lib, 8 GB yoki 12 GB operativ xotira va 256 GB ichki xotira variantlari mavjud. Asosiy kamerasi 108 MP (asosiy), 5 MP (ultrakeng), 2 MP (makro), old kamerasi esa 50 MP. 4500 mAh sig‘imli batareya 35W tezkor zaryadlashni qo‘llab-quvvatlaydi. Qurilma Android 14 operatsion tizimida ishlaydi.`,
-    pictureLink: 'https://images.uzum.uz/cpeoq1jmdtjnp7383o9g/t_product_low.jpg'
+    pictureLink: 'https://images.uzum.uz/cpeoq1jmdtjnp7383o9g/t_product_low.jpg',
+    isAddedToCart:false
   },
 };
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { phonesData } from './db';
+import { boxesWrapper } from './elements';
+
+document.addEventListener('DOMContentLoaded', function () {
+  function createBox() {
+    for (const key in phonesData) {
+      if (phonesData.hasOwnProperty(key)) {
+        const phone = phonesData[key as keyof typeof phonesData];
+
+        const box = document.createElement('div');
+        box.classList = 'flex w-full flex-col items-center overflow-hidden rounded-md bg-gray-300 pb-5';
+
+        const img = document.createElement('img');
+        img.setAttribute('src', `${phone.pictureLink}`);
+        box.appendChild(img);
+
+        const bottomWrapper = document.createElement('div');
+        bottomWrapper.className = 'mt-4 flex w-full flex-col items-start px-3';
+        bottomWrapper.innerHTML = `
+        <h1 class="font-poppins text-[22px]">${phone.showName}</h1>
+        <p class="mt-2 font-poppins text-[18px]">${phone.showPrice}</p>`;
+
+        const buttonsWrapper = document.createElement('div');
+        buttonsWrapper.className = 'mt-8 flex w-full items-center justify-between';
+        buttonsWrapper.innerHTML = `
+            <a href=""><button class="rounded-md bg-green-500 px-2 py-1 text-white">Information</button></a>
+              ${phone.isAddedToCart? `<a href=""><button class="rounded-md bg-yellow-500 px-2 py-1 text-white">Retrieve</button></a>` :`<a href=""><button class="rounded-md bg-blue-500 px-2 py-1 text-white">Add to Cart</button></a>`}`;
+        bottomWrapper.appendChild(buttonsWrapper);
+        box.appendChild(bottomWrapper);
+        boxesWrapper?.appendChild(box);
+      }
+    }
+  }
+
+  function init() {
+    createBox();
+  }
+
+  init();
+});
